c4cb15d89cdeffe8ae81bbd9835feba266d1e92a
Rikuto <mikadorikuto@gmail.com>
2026-09-17 19:04:16 +0900

    updated the code

--- src/calc_exomoons.py
+++ src/calc_exomoons.py
@@ -2,11 +2,12 @@ import pandas as pd
 import numpy as np
 from astropy import units as u
 from astropy import constants as const
+import os
 
 
 def process_exomoons():
     # Loading raw data
-    input_path = "data/raw/jovian_planets_raw.csv"
+    input_path = "data/jovian_planets_raw.csv"
     try:
         df = pd.read_csv(input_path)
         print(f"Successfully loaded {len(df)} planets.")
@@ -24,8 +25,33 @@ def process_exomoons():
     # Calculating Hill Radius
     mass_ratio = (m_p / (3 * m_s)).decompose()
     hill_radius_au = a * (mass_ratio ** (1 / 3))
+
     df["hill-radius_AU"] = hill_radius_au.value
     df["hill_radius_km"] = hill_radius_au.to(u.km).value
+
+    # Filtering for Habitability (Temperature)
+    min_temp = 250
+    max_temp = 350
+
+    hz_candidates = df[(df["pl_eqt"] >= min_temp) & (df["pl_eqt"] <= max_temp)].copy()
+    hz_candidates["est_temp_C"] = hz_candidates["pl_eqt"] - 273.15
+
+    print(
+        f"Found {len(hz_candidates)} potentional Exomoon host candidates in the Habitable Zone!"
+    )
+
+    # Formatting and Saving to CSV
+    output_columns = [
+        "pl_name",
+        "hostname",
+        "pl_bmassj",
+        "pl_orbsmax",
+        "pl_orbeccen",
+        "pl_eqt",
+        "est_temp_C",
+        "hill_radius_AU",
+        "hill_radius_km",
+    ]
 
     # For debugging
     print("\n[Preview of calculated Hill Radii]")
